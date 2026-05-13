8de7732906c9b4bfd5e69834a218758f0cd9e46b
Amna <amnalbalushi81@gmail.com>
2026-05-13 15:12:43 +0400

    Update Trip.dart

--- lib/model/Trip.dart
+++ lib/model/Trip.dart
@@ -8,9 +8,9 @@ class Trip {                    //for the trips the user has
   final String status;        //each trip is a planning, or active or history trip
   final String imageUrl;      //network photo (existing trips)
   final String imagePath;     //local file photo (user-picked from gallery)
-
+  final String firebaseKey;   //link the database
   Trip({required this.name,required this.dates,required this.cities,
     required this.days, required this.flag,required this.status,
-    this.imageUrl = '', this.imagePath = '',
+    this.imageUrl = '', this.imagePath = '', this.firebaseKey= '',
   });
 }
